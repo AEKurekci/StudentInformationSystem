195dd4627cd904ac95c4d3395bdccf987bb438a9
Ali Emre Kürekci <aliemrekurekci@gmail.com>
2019-12-02 11:36:36 +0300

    update

--- app/src/main/java/com/example/studentinformationsystem/StudentEntry.java
+++ app/src/main/java/com/example/studentinformationsystem/StudentEntry.java
@@ -2,13 +2,25 @@ package com.example.studentinformationsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StudentEntry extends AppCompatActivity {
-
+    Button btnEntry;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_entry);
+
+        btnEntry = findViewById(R.id.btnEntry);
+        btnEntry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(StudentEntry.this, MainPage.class);
+                startActivity(i);
+            }
+        });
     }
 }
