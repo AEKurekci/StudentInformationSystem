2f91347abb865ba3fa344db1506e3c13286e48e9
akboga <hiakboga@gmail.com>
2019-12-16 08:03:43 +0200

    random

--- app/src/main/java/com/example/studentinformationsystem/StudentEntry.java
+++ app/src/main/java/com/example/studentinformationsystem/StudentEntry.java
@@ -4,15 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class StudentEntry extends AppCompatActivity {
     Button btnEntry;
+    ImageButton randomButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_entry);
+
+        final TextView sayilarToplami  = (TextView) findViewById(R.id.textView6);
 
         btnEntry = findViewById(R.id.btnEntry);
         btnEntry.setOnClickListener(new View.OnClickListener() {
@@ -22,5 +30,21 @@ public class StudentEntry extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String bilgi = "";
+                Random randomSayiOlusturucu = new Random();
+
+                Integer randomSayi = randomSayiOlusturucu.nextInt(99);
+                Integer randomSayi2 = randomSayiOlusturucu.nextInt(99);
+
+                bilgi = randomSayi.toString() +" + "+ randomSayi2.toString();
+                sayilarToplami.setText(bilgi);
+            }
+        };
+        randomButton = findViewById(R.id.refreshButton);
+        randomButton.setOnClickListener(listener);
     }
 }
