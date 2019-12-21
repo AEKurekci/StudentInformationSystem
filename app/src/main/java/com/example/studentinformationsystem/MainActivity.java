d109a95a2710d8b38a18eb2022aef766c96de6bb
Ali Emre Kürekci <aliemrekurekci@gmail.com>
2019-12-21 14:47:51 +0300

    added academist entry activity

--- app/src/main/java/com/example/studentinformationsystem/MainActivity.java
+++ app/src/main/java/com/example/studentinformationsystem/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean startFromAuthentication = false;
     Button btnStd;
+    Button btnAca;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +28,15 @@ public class MainActivity extends AppCompatActivity {
                     Intent intent = new Intent(MainActivity.this, MainPage.class);
                     startActivity(intent);
                 }
+            }
+        });
+
+        btnAca = findViewById(R.id.btnAcd);
+        btnAca.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AcademistEntry.class);
+                startActivity(intent);
             }
         });
     }
